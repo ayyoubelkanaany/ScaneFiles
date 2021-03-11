c4dafb15452ded332b68ae86c1ef2201ecb5ebbc
ayyoubelkanaany <elkanaany.21@gmail.com>
2021-03-11 19:41:46 +0100

    after search work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   files: Array<File> = new Array<File>();
+  filesRef: Array<File> = new Array<File>();
   title = 'ScanFiles';
   good = false;
   analyse = false;
   handleFileInput(files: FileList){
     this.files.push(files.item(0));
+    this.filesRef = this.files;
     console.log( files.item(0).size);
   }
   analyser(file){
     ///traitemen d'analyse puis le resultat
     this.analyse = true;
   }
-  filter(){
-
+  filter(nom: string){
+  this.files = this.filesRef;
+  if(nom !== ''){
+    this.files = this.filesRef.filter(value => value.name.startsWith(nom));
   }
+  else{
+    this.files = this.filesRef;
+    }
+  }
+
 }
